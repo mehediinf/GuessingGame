4944f0453f2b281b4797ce38e39a4585635d9935
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-02-06 01:25:23 +0600

    Second Commit GuessingGame

--- app/src/main/java/com/example/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -3,12 +3,76 @@ package com.example.guessinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+    private Button checkButton;
+    private TextView resultTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText_Id);
+        checkButton = findViewById(R.id.checkButton_Id);
+        resultTextView = findViewById(R.id.resultTextView_Id);
+
+        checkButton.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        String guessString =editText.getText().toString();
+
+        if (guessString.isEmpty())
+        {
+            errorMessage();
+        }
+        else {
+
+            int guess = Integer.parseInt(guessString);
+
+            if (guess>5){
+                errorMessage();
+            }
+            else {
+                Random random = new Random();
+
+                int randomNumber = random.nextInt( 5)+1;
+
+                if (randomNumber==guess){
+
+                    resultTextView.setText("Congrats !!! You have won");
+                }
+                else {
+
+                    resultTextView.setText("Sorry ! You have lost. Random number was "+randomNumber);
+                }
+            }
+
+
+        }
+
+
+    }
+
+    void errorMessage()
+    {
+        editText.setError("Please enter an valid input");
+        editText.requestFocus();
+        return;
+    }
+
 }
